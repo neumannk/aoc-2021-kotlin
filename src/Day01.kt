44c7c0142846397331bb40d447739e3138cf7a93
Kevin Neumann <kevin@kneumann.org>
2021-12-01 21:26:25 -0800

    day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,36 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        // this approach is sort of neat, but a naive test shows it's about 3x slower than what's below
+        /*
+        return input.map { it.toInt() }
+            .windowed(2)
+            .count { it.last() > it.first() }
+         */
+        var increasing = 0
+        for (i in 1 until input.size) {
+            if (input[i].toInt() > input[i - 1].toInt()) ++increasing
+        }
+        return increasing
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        // no way I'm not using windowed() for this part...
+        val windowSums = input.map { it.toInt() }
+            .windowed(size = 3, partialWindows = false)
+            .map { it.sum() }
+        var increasing = 0
+        for (i in 1 until windowSums.size) {
+            if (windowSums[i] > windowSums[i - 1]) ++increasing
+        }
+        return increasing
     }
 
-    // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
     val input = readInput("Day01")
+
+    check(part1(testInput) == 7)
     println(part1(input))
+
+    check(part2(testInput) == 5)
     println(part2(input))
 }
